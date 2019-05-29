9b8bc1bef7c8088149ca02d69aa4184d79433d0f
sobhaniiest <sohaniiest@gmail.com>
2019-05-29 10:31:47 +0530

    Comments are added

--- killtimer.c
+++ killtimer.c
@@ -8,15 +8,19 @@ guint timer;
 
 void KillTimer(GSourceFunc killfunc)
 {
-	timeout = 30;
+	timeout = 30; /* default value */
 	kill_func = killfunc;
 	holds = 0;
 	add_timeout();
-	pthread_mutex_init(&lock, NULL);
+	pthread_mutex_init(&lock, NULL); /* intitializes a mutex */
 }
 
 void add_timeout()
 {
+	/* 
+	sets a function (kil) to be called at regular 
+	interval (timeout) until the function returns False
+	*/
 	timer = g_timeout_add_seconds(timeout, (void *)kil, NULL);
 }
 
@@ -31,14 +35,14 @@ void kil()
 
 void add_hold()
 {
-	pthread_mutex_lock(&lock);
+	pthread_mutex_lock(&lock); /* Locks a mutex object */
 	if(holds == 0)
 	{
 		fprintf(stderr, "Kill timer stopped\n");
 		g_source_remove(timer);
 	}
 	holds += 1;
-	pthread_mutex_unlock(&lock);
+	pthread_mutex_unlock(&lock); /* Releases a mutex object */
 }
 
 void remove_hold()
@@ -61,6 +65,7 @@ void alive()
 	pthread_mutex_lock(&lock);
 	if(holds == 0)
 	{
+		/* Removes the event source specified by tag (timer) */
 		g_source_remove(timer);
 		add_timeout();
 	}
--- killtimer.h
+++ killtimer.h
@@ -1,8 +1,11 @@
 #ifndef KILLTIMER_H
 #define KILLTIMER_H
 
+/* General Header file - fprintf() : debuging purpose */
 #include <stdio.h>
+/* GLib methods and data types */
 #include <glib.h>
+/* Thread mutex lock and unlock : to avoid critical section problem */
 #include <pthread.h>
 
 extern GSourceFunc killfunc, kill_func;
