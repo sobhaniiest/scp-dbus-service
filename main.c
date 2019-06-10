07359a688fb76bb832bf54b289edb4c70e7f91bb
sobhaniiest <sobhaniiest@gmail.com>
2019-06-11 03:56:38 +0530

    minor changes

--- main.c
+++ main.c
@@ -1,16 +1,20 @@
-
 /*
-For checking and testing purpose 
+*** The main program is for checking and testing purpose 
 */
 #include <python3.6m/Python.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <glib.h>
+#include <string.h>
+#include <cups/cups.h>
 #include <pthread.h>
 #include <gtk/gtk.h>
 #include "killtimer.h"
 #include "ppdcache.h"
 #include "asyncconn.h"
 #include "authinfocache.h"
+#include "ConfigPrintingNewPrinterDialog.h"
+#include "newprinterdialog_dbus.h"
 
 typedef struct _dict_main
 {
@@ -49,12 +53,10 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-/*
-gcc `pkg-config --cflags gtk+-3.0` main.c killtimer.c `pkg-config --cflags --libs glib-2.0 --libs gtk+-3.0` -o main
-*/
 
-/*
+// gcc `pkg-config --cflags gtk+-3.0` main.c killtimer.c `pkg-config --cflags --libs glib-2.0 --libs gtk+-3.0` -o main
 
-gcc `pkg-config --cflags gtk+-3.0` main.c ppdcache.c killtimer.c `pkg-config --cflags --libs glib-2.0 --libs gtk+-3.0` -lcups -lpython3.6m  -o main
 
-*/
+
+// gcc `pkg-config --cflags gtk+-3.0` main.c ppdcache.c killtimer.c `pkg-config --cflags --libs glib-2.0 --libs gtk+-3.0` -lcups -lpython3.6m  -o main
+
