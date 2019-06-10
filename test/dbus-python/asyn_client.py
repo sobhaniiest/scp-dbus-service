9647710891daa9f466365170e3d59629143c0094
sobhaniiest <sobhaniiest@gmail.com>
2019-06-11 00:57:03 +0530

    testing GDBus and dbus-python methos and signals

---
+++ test/dbus-python/asyn_client.py
@@ -0,0 +1,88 @@
+import sys
+import traceback
+
+from gi.repository import GLib
+
+import dbus
+import dbus.mainloop.glib
+
+# Callbacks for asynchronous calls
+
+def handle_add_reply(s):
+    global add_replied
+    add_replied = True
+
+    print("sum = ", int(s))
+
+    if add_replied and raise_replied:
+        loop.quit()
+
+def handle_add_error(e):
+    global failed
+    global add_replied
+    hello_replied = True
+    failed = True
+
+    print("add raised an exception! That's not meant to happen...")
+    print(str(e))
+
+    if add_replied and raise_replied:
+        loop.quit()
+
+def handle_raise_reply():
+    global failed
+    global raise_replied
+    raise_replied = True
+    failed = True
+
+    print("RaiseException returned normally! That's not meant to happen...")
+
+    if add_replied and raise_replied:
+        loop.quit()
+
+def handle_raise_error(e):
+    global raise_replied
+    raise_replied = True
+
+    print("RaiseException raised an exception as expected:")
+    print(str(e))
+
+    if add_replied and raise_replied:
+        loop.quit()
+
+def make_calls():
+    # To make an async call, use the reply_handler and error_handler kwargs
+    remote_object.add(2, 3,
+                    dbus_interfac='com.test.CalculatorInterface',
+                    reply_handler=handle_add_reply,
+                    error_handler=handle_add_error)
+
+    # Interface objects also support async calls
+    iface = dbus.Interface(remote_object, 'com.test.CalculatorInterface')
+    
+    iface.RaiseException(reply_handler=handle_raise_reply,
+                         error_handler=handle_raise_error)
+
+    return False
+
+if __name__ == '__main__':
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default = True)
+
+    bus = dbus.SessionBus()
+    try:
+        remote_object = bus.get_object("com.test.CalculatorService","/Calculate")
+    except dbus.DBusException:
+        traceback.print_exc()
+        sys.exit(1)
+
+    # Make the method call after a short delay
+    GLib.timeout_add(1000, make_calls)
+
+    failed = False
+    hello_replied = False
+    raise_replied = False
+
+    loop = GLib.MainLoop()
+    loop.run()
+    if failed:
+        raise SystemExit("test async client failed!")
---
+++ test/dbus-python/server.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+
+from gi.repository import GLib
+
+import dbus
+import dbus.service
+import dbus.mainloop.glib
+
+class DemoException(dbus.DBusException):
+    _dbus_error_name = "com.test.DemoException"
+
+class Calculate(dbus.service.Object):
+
+    @dbus.service.method("com.test.CalculatorInterface", in_signature='ii', out_signature='i')  
+    def add(self, a, b):
+        s = int(a) + int(b)
+        print(int(a)," + ", int(b), " = ", s)
+        return s
+
+    @dbus.service.method("com.test.CalculatorInterface", in_signature='ii', out_signature='i')  
+    def sub(self, a, b):
+        s = int(a) - int(b)
+        print(int(a)," - ", int(b), " = ", s)
+        return s
+
+    @dbus.service.method("com.test.CalculatorInterface", in_signature='ii', out_signature='i')  
+    def multi(self, a, b):
+        s = int(a) * int(b)
+        print(int(a)," * ", int(b), " = ", s)
+        return s
+
+    @dbus.service.method("com.test.CalculatorInterface", in_signature='', out_signature='')
+    def RaiseException(self):
+        raise DemoException("The RaiseException method !!!")
+
+    @dbus.service.method("com.test.CalculatorInterface", in_signature='', out_signature='')
+    def Exit(self):
+        mainloop.quit()
+
+if __name__ == '__main__':
+
+    # main loop to allow asynchronous method calls
+    dbus.mainloop.glib.DBusGMainLoop(set_as_default = True)
+
+    # connect to the session bus by creating a session bus object 
+    session_bus = dbus.SessionBus()
+
+    # A base class for exporting your own Named Services 
+    # "com.test.CalculatorService" across the Bus.
+    name = dbus.service.BusName("com.test.CalculatorService", session_bus)
+    
+    object = Calculate(session_bus, "/Calculate")
+
+    print("Server is running !!!")
+
+    # GLib will run the callbacks when appropriate
+    mainloop = GLib.MainLoop()
+    mainloop.run()
