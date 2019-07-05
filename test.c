964833993df0f8557553af10bc8b0a5f39929443
sobhaniiest <sobhaniiest@gmail.com>
2019-07-05 23:51:07 +0530

    for testing

--- test.c
+++ test.c
@@ -1,17 +1,30 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <glib.h>
 #include <string.h>
 #include <cups/cups.h>
 #include <pthread.h>
 #include <gtk/gtk.h>
 #include <errno.h>
+#include <libgen.h>
+#include <unistd.h>
+#include <cups/ppd.h>
+#include <ctype.h>
+#include <iconv.h>
+#include <stdbool.h>
+#include <cups/language.h>
+#include <cups/adminutil.h>
+/*
 #include "killtimer.h"
 #include "ppdcache.h"
 #include "asyncconn.h"
 #include "authinfocache.h"
 #include "newprinter.h"
 #include "ConfigPrintingNewPrinterDialog.h"
+*/
+#include "MissingExecutables.h"
 #include "newprinterdialog_dbus.h"
 
 int main()
@@ -20,7 +33,7 @@ int main()
     const char *host = cupsServer();
     int port = ippPort();
     http_encryption_t encryption = cupsEncryption();
-    */
+    
     printer_uri *status = Async_Connection(NULL,
                                            NULL,
                                            NULL,
@@ -31,17 +44,23 @@ int main()
                                            true,
                                            "getPPDs");
 
-/*
+
     while(status != NULL)
     {
         fprintf(stderr, "%s %s\n", status->name, status->uri);
         status = status->next;
     }
-*/
+
     while(status != NULL)
     {
         fprintf(stderr, "%s\n", status->name);
         status = status->next;
     }
+    */
+
+    const char *ppd_filename = "Generic-PDF_Printer-PDF.ppd";
+    char *result = missingexecutables(ppd_filename);
+    fprintf(stderr, "%s\n",result);
+
 
 }
