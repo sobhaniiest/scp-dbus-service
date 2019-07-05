e7c91c30a0d2028f1d4e01d4b1a3e688f5077300
sobhaniiest <sobhaniiest@gmail.com>
2019-07-05 23:37:55 +0530

    added MissingExecutables

--- ConfigPrinting.c
+++ ConfigPrinting.c
@@ -2,19 +2,13 @@
 #include <stdlib.h>
 #include <glib.h>
 #include <string.h>
-#include <cups/cups.h>
-#include <pthread.h>
 #include <gtk/gtk.h>
-#include <errno.h>
 #include "killtimer.h"
-#include "ppdcache.h"
-#include "asyncconn.h"
-#include "authinfocache.h"
-#include "newprinter.h"
 #include "ConfigPrintingNewPrinterDialog.h"
-#include "newprinterdialog_dbus.h"
+#include "MissingExecutables.h"
+#include "scp_interface.h"
 
-#define NPD NewPrinterDialogDBusOrgFedoraprojectConfigPrinting
+#define scpinterface scpinterfaceOrgFedoraprojectConfigPrinting
 
 GDBusConnection *conn;
 const gchar *name = "org.fedoraproject.Config.Printing";
@@ -25,10 +19,36 @@ static void name_acquired_handler(GDBusConnection *connection,
 	                              const gchar *name, 
 	                              gpointer user_data);
 
-static gboolean NewPrinterDialog(NPD *interface,
+static gboolean NewPrinterDialog(scpinterface *interface,
 								 GDBusMethodInvocation *invocation,
 								 gpointer user_data);
+/*
+static gboolean PrinterPropertiesDialog(scpinterface *interface,
+								        GDBusMethodInvocation *invocation,
+								        guint xid,
+								        const gchar *name,
+								        gpointer user_data);
 
+static gboolean JobApplet(scpinterface *interface,
+						  GDBusMethodInvocation *invocation,
+						  gpointer user_data);
+
+static gboolean GetBestDrivers(scpinterface *interface,
+							   GDBusMethodInvocation *invocation,
+							   const gchar *device_id,
+							   const gchar *device_make_and_model,
+							   const gchar *device_uri,
+							   gpointer user_data);
+*/
+static gboolean MissingExecutables(scpinterface *interface,
+								   GDBusMethodInvocation *invocation,
+								   const gchar *ppd_filename,
+								   gpointer user_data);
+/*
+static gboolean GroupPhysicalDevices(scpinterface *interface,
+								     GDBusMethodInvocation *invocation,
+								     gpointer user_data);
+*/
 int main()
 {
 	GMainLoop *loop;
@@ -51,7 +71,7 @@ static void name_acquired_handler(GDBusConnection *connection,
 	                              gpointer user_data)
 {
 	conn = connection;
-	NPD *interface;
+	scpinterface *interface;
 	GError *error;
 
 	/* main initialization */
@@ -59,21 +79,46 @@ static void name_acquired_handler(GDBusConnection *connection,
     
 	/**********************/
 
-	interface = newprinterdialog_dbus_org_fedoraproject_config_printing_skeleton_new();
+	interface = scp_interface_org_fedoraproject_config_printing_skeleton_new();
 
 	g_signal_connect(interface, 
 					 "handle-new-printer-dialog", 
 					 G_CALLBACK(NewPrinterDialog), 
 					 NULL);
-	
+	/*
+	g_signal_connect(interface, 
+					 "handle-printer-properties-dialog", 
+					 G_CALLBACK(PrinterPropertiesDialog), 
+					 NULL);
+	g_signal_connect(interface, 
+					 "handle-job-applet", 
+					 G_CALLBACK(JobApplet), 
+					 NULL);
+	g_signal_connect(interface, 
+					 "handle-get-best-drivers", 
+					 G_CALLBACK(GetBestDrivers), 
+					 NULL);
+	*/
+	g_signal_connect(interface, 
+					 "handle-missing-executables", 
+					 G_CALLBACK(MissingExecutables), 
+					 NULL);
+	/*
+	g_signal_connect(interface, 
+					 "handle-group-physical-devices", 
+					 G_CALLBACK(GroupPhysicalDevices), 
+					 NULL);
+	*/
 	error = NULL;
 	g_dbus_interface_skeleton_export(G_DBUS_INTERFACE_SKELETON(interface), 
 									 connection, 
 									 "/org/fedoraproject/Config/Printing", 
 									 &error);
+
+
 }
 
-static gboolean NewPrinterDialog(NPD *interface,
+static gboolean NewPrinterDialog(scpinterface *interface,
 								 GDBusMethodInvocation *invocation,
 								 gpointer user_data)
 {
@@ -81,7 +126,66 @@ static gboolean NewPrinterDialog(NPD *interface,
 	snprintf(path, 1024, "/org/fedoraproject/Config/Printing/NewPrinterDialog/%d", pathn);
 	CPNewPrinterDialog(conn, name, path);
 	alive();
-	newprinterdialog_dbus_org_fedoraproject_config_printing_complete_new_printer_dialog(interface, invocation, path);
-	return FALSE;
+	scp_interface_org_fedoraproject_config_printing_complete_new_printer_dialog(interface, invocation, path);
+	return TRUE;
+}
+/*
+static gboolean PrinterPropertiesDialog(scpinterface *interface,
+								        GDBusMethodInvocation *invocation,
+								        guint xid,
+								        const gchar *name,
+								        gpointer user_data)
+{
+	pathn += 1;
+	snprintf(path, 1024, "/org/fedoraproject/Config/Printing/PrinterPropertiesDialog/%d", pathn);
+	CPPrinterPropertiesDialog(conn, path, xid, name);
+	alive();
+	scp_interface_org_fedoraproject_config_printing_complete_printer_properties_dialog(interface, invocation, path);
+	return TRUE;
 }
 
+static gboolean JobApplet(scpinterface *interface,
+						  GDBusMethodInvocation *invocation,
+						  gpointer user_data)
+{
+	pathn += 1;
+	snprintf(path, 1024, "/org/fedoraproject/Config/Printing/JobApplet/%d", pathn);
+	CPJobApplet(conn, path);
+	alive();
+	scp_interface_org_fedoraproject_config_printing_complete_job_applet(interface, invocation, path);
+	return TRUE;
+}
+
+static gboolean GetBestDrivers(scpinterface *interface,
+							   GDBusMethodInvocation *invocation,
+							   const gchar *device_id,
+							   const gchar *device_make_and_model,
+							   const gchar *device_uri,
+							   gpointer user_data)
+{
+	GVariant *drivers = GetBestDriversRequest(device_id, device_make_and_model, device_uri);
+	scp_interface_org_fedoraproject_config_printing_complete_get_best_drivers(interface, invocation, drivers);
+	return TRUE;
+}
+*/
+static gboolean MissingExecutables(scpinterface *interface,
+								   GDBusMethodInvocation *invocation,
+								   const gchar *ppd_filename,
+								   gpointer user_data)
+{
+	const gchar *missing_executables = missingexecutables(ppd_filename);
+	scp_interface_org_fedoraproject_config_printing_complete_missing_executables(interface, 
+																				 invocation, 
+																				 missing_executables);
+	return TRUE;
+}
+/*
+static gboolean GroupPhysicalDevices(scpinterface *interface,
+								 	 GDBusMethodInvocation *invocation,
+								     gpointer user_data)
+{
+	GVariant *grouped_devices = GroupPhysicalDevicesRequest();
+	scp_interface_org_fedoraproject_config_printing_complete_group_physical_devices(interface, invocation, grouped_devices);
+	return TRUE;
+}
+*/
