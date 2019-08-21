6665eaa4cf7e2f2de2c3e4310542361175caf452
sobhaniiest <sobhaniiest@gmail.com>
2019-08-21 19:47:59 +0530

    for testing the library

--- demo/test.c
+++ demo/test.c
@@ -1,66 +1,54 @@
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <glib.h>
+#include <stdio.h> 
 #include <string.h>
-#include <cups/cups.h>
-#include <pthread.h>
-#include <gtk/gtk.h>
-#include <errno.h>
-#include <libgen.h>
-#include <unistd.h>
-#include <cups/ppd.h>
-#include <ctype.h>
-#include <iconv.h>
-#include <stdbool.h>
-#include <cups/language.h>
-#include <cups/adminutil.h>
-/*
-#include "killtimer.h"
-#include "ppdcache.h"
-#include "asyncconn.h"
-#include "authinfocache.h"
-#include "newprinter.h"
-#include "ConfigPrintingNewPrinterDialog.h"
-*/
-#include "MissingExecutables.h"
-#include "scp_interface.h"
+#include <stdlib.h>
+#include <SCPService.h>
 
-int main()
+int main(int argc, char *argv[])
 {
     /*
-    const char *host = cupsServer();
-    int port = ippPort();
-    http_encryption_t encryption = cupsEncryption();
+    if (argc != 6)
+    {
+        printf("Usage : %s \n", argv[0]);
+        printf("\t'xid'\n");
+        printf("\t'device_uri'\n");
+        printf("\t'device_id'\n");
+        printf("\t'printer_name'\n"); 
+        printf("\t'ppd_file_name'\n");
+        exit(EXIT_SUCCESS);
+    }
+
+    GMainLoop *loop = g_main_loop_new(NULL, FALSE);
+    KillTimer(gtk_main_quit);
+
+    unsigned int xid = (unsigned int) atoi(argv[1]);
+    const char *device_uri = argv[2];
+    const char *device_id = argv[3];
+    const char *name = argv[4];
+    const char *file_name = argv[5];    
     
-    printer_uri *status = Async_Connection(NULL,
-                                           NULL,
-                                           NULL,
-                                           "\0",
-                                           0,
-                                           0,
-                                           true,
-                                           true,
-                                           "getPPDs");
+    alive();
 
+    printf("Calling - NewPrinterFromDevice (xid, device_uri, device_id) \n");
+    NewPrinterFromDevice(xid, device_uri, device_id);
+    remove_hold();
 
-    while(status != NULL)
-    {
-        fprintf(stderr, "%s %s\n", status->name, status->uri);
-        status = status->next;
-    }
+    printf("Calling - DownloadDriverForDeviceID (xid, device_id) \n");
+    DownloadDriverForDeviceID(xid, device_id);
+    remove_hold();
 
-    while(status != NULL)
-    {
-        fprintf(stderr, "%s\n", status->name);
-        status = status->next;
-    }
+    /*
+    printf("Calling - ChangePPD(xid, printer_name, device_id) \n");
+    ChangePPD(xid, name, device_id);
+    remove_hold();
+
+    printf("Calling - PrintTestPage (xid, printer_name) \n");
+    PrintTestPage((unsigned int)atoi(argv[1]), argv[2]);
+    remove_hold();
     */
 
-    const char *ppd_filename = "Generic-PDF_Printer-PDF.ppd";
-    char *result = missingexecutables(ppd_filename);
-    fprintf(stderr, "%s\n",result);
+    printf("Calling - MissingExecutables(ppd_filename) \n");
+    char **result = missingexecutables("Generic-PDF_Printer-PDF.ppd");
+    printf("###  %s\n", result[0]);
 
-
+    //g_main_loop_run(loop);
 }
